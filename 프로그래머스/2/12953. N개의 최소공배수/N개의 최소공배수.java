6f6a7cbf06a87ca91602fb9c795bf3f883c8ab98
LEE JIEUN <104649081+ljinny@users.noreply.github.com>
2024-11-03 02:10:55 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            answer = lcm(answer, arr[i]);
+        }
+        
+        return answer;
+    }
+ 
+    public int gcd(int n, int m) {
+        while (m != 0) {
+            int temp = m;
+            m = n % m;
+            n = temp;
+        }
+        return n;
+    }
+
+    public int lcm(int n, int m) {
+        return n * m / gcd(n, m);
+    }
+}
